d4184bb014d10c676050b89ef098705dd1df860c
Your Name <you@example.com>
2023-11-08 15:19:29 -0500

    cambios3

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -13,3 +13,38 @@ const validateUser = ()=>{
   
 }
 validateUser();
+const tableShow = window.location.search.split('=')[1];
+const tableSection = document.querySelector('#tableSection');
+
+console.log(tableSection);
+
+fetch(`./data/${tableShow}.json`)
+.then( response => response.json())
+.then( data => {
+
+    // console.log(Object.keys())
+    tableSection.innerHTML = `
+    <table class="table table-striped" id="myTable">
+        <thead>
+            <tr>
+                ${Object.keys(data[0]).map( key => `<th scope="col">${key.toUpperCase()}</th>`).join('')}
+            </tr>
+        </thead>
+        <tbody>
+            ${data.map( row => `
+                <tr>
+                    ${Object.keys(row).map( cell => `<td>${row[cell]}</td>`).join('')}
+                </tr>
+            `).join('')}
+        
+        <tbody>
+        </tbody>
+    </table>
+    `
+
+    let table = new DataTable('#myTable', {
+        // options
+    });
+
+    
+})
